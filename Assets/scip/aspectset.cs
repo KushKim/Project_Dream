b64c7f5bbbe382cd0e68a47e04e78ea64681215f
KushKim <ds12000@naver.com>
2020-11-09 19:17:11 +0900

    16:10 고정비율 스크립트 테스트중 (적용가능한 고정비율이있다면 적용후 커밋올려주세요!)

---
+++ Assets/scip/aspectset.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aspectset : MonoBehaviour
+{
+    private void setupCamera()
+    {
+        float targetWidthAspect = 16.0f;
+
+        float targetHeightAspect = 10.0f;
+
+        Camera mainCamera = Camera.main;
+
+        mainCamera.aspect = targetWidthAspect / targetHeightAspect;
+
+        float widthRatio = (float)Screen.width / targetWidthAspect;
+        float heightRatio = (float)Screen.height / targetHeightAspect;
+
+        float heightadd = ((widthRatio / (heightRatio / 100)) - 100) / 200;
+        float widthadd = ((heightRatio / (widthRatio / 100)) - 100) / 200;
+
+        if (heightRatio > widthRatio)
+        {
+            widthadd = 0.0f;
+        }
+        else
+        { heightadd = 0.0f; }
+
+        mainCamera.rect = new Rect(
+            mainCamera.rect.x + Math.Abs(widthadd),
+            mainCamera.rect.y + Math.Abs(heightadd),
+            mainCamera.rect.width + (widthadd * 2),
+            mainCamera.rect.height + (heightadd * 2));
+    }
+}
