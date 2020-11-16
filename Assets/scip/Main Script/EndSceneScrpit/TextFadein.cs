554d9044ef3478ecade3306fbfb18712e5b7a3d7
KushKim <ds12000@naver.com>
2020-11-16 17:41:28 +0900

    *변경사항*
    EndScene추가 완료 및 이미지애니메이션 추가 완료
    엔딩크레딧 임시로 로고적용완료

---
+++ Assets/scip/Main Script/EndSceneScrpit/TextFadein.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextFadein : MonoBehaviour
+{
+    public GameObject Text;
+    public int ShowDelay;
+
+    private void Start()
+    {
+        Invoke("TextDelay", ShowDelay);
+    }
+
+    void TextDelay()
+    {
+        Text.gameObject.SetActive(true);
+    }
+}
