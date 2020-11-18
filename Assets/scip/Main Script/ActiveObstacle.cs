95b33404e45e81ed5781768c0ddf2e004389d3a9
Hyangyoun <thakd7092@naver.com>
2020-11-18 11:49:32 +0900

    장애물 작동 구현중(미완)

---
+++ Assets/scip/Main Script/ActiveObstacle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveObstacle : MonoBehaviour
+{
+    private bool active;
+    public float downSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
+        Debug.DrawRay(transform.position, Vector2.down * 4f, Color.red);
+        if (active)
+        {
+            if(hit.collider != null)
+            {
+                Debug.Log(hit.collider.name);
+                transform.Translate(Vector2.down * downSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            active = true;
+        }
+    }
+}
