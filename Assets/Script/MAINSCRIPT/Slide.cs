80f39fd020df219cfc29dcb49dbabdde10556c2a
JungheunKim <kushkim@icloud.com>
2020-12-01 02:49:25 +0900

    Create MainScript
    씬별 스크립트가아닌 메인게임을 위한 스크립트 정리

---
+++ Assets/Script/MAINSCRIPT/Slide.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slide : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+    public Animator anim;
+    public BoxCollider2D box;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        anim.SetBool("Slide",true);
+        box.size = new Vector2(11.81f, 9.33f);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        anim.SetBool("Slide", false);
+        box.size = new Vector2(8.330201f, 13.4302f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
