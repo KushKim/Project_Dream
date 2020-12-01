1dc49bbca9969dcb04c60de3f2c958c0136771fd
Hyangyoun <thakd7092@naver.com>
2020-12-01 22:30:02 +0900

    플레이어 관련 스크립트 갈아엎는중

---
+++ Assets/Script/PlayerScript/PlayerMove.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class Player : MonoBehaviour // 플레이어 상태 클래스
+{
+    static public int playerHP = 3;
+    public bool infinityPlayer;
+    static private BoxCollider2D box;
+    static private SpriteRenderer sp;
+
+    private void Start()
+    {
+       box = GetComponent<BoxCollider2D>();
+        sp = GetComponent<SpriteRenderer>();
+    }
+    public IEnumerator Hit(IEnumerator coroutine)
+    {
+        if (!infinityPlayer)
+        {
+            --playerHP;
+            infinityPlayer = true;
+            StartCoroutine(coroutine);
+            yield return new WaitForSeconds(2f);
+            infinityPlayer = false;
+            StopCoroutine(coroutine);
+        }
+    }
+    
+    public IEnumerator Infinity()
+    {
+        box.enabled = false;
+        yield return new WaitForSeconds(0.3f);
+        box.enabled = true;
+        while (true)
+        {
+            sp.color = new Color(255, 255, 255, 0);
+            yield return new WaitForSeconds(0.3f);
+            sp.color = new Color(255, 255, 255, 255);
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+}
+public class PlayerMove : MonoBehaviour
+{
+    [Header("움직임 관련 수치")]
+    public float moveSpeed = 8.5f;
+    public float jumpPower = 6f;
+    public int jumpCount = 2;
+
+    private Transform Pos;
+    private Rigidbody2D rigd;
+    private BoxCollider2D box;
+
+    private Animator anim;
+    private Player player = new Player();
+
+    void Start()
+    {
+       Pos = GetComponent<Transform>();
+       rigd = GetComponent<Rigidbody2D>();
+       anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pos.Translate(Vector2.right * moveSpeed * Time.deltaTime); // 기본 전진
+        if (Pos.position.y < -7) // 떨어진 좌표
+        {
+            rigd.velocity = new Vector2(0, 20f);
+            StartCoroutine(player.Hit(player.Infinity()));
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag.Equals("Ground"))
+        {
+            jumpCount = 2;
+            anim.SetBool("JumpOn", false);
+        }
+
+        if (collision.gameObject.tag.Equals("Obstacle"))
+        {
+            collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+    public void Jump()
+    {
+        if (jumpCount > 0)
+        {
+            anim.SetInteger("Jump", jumpCount);
+            anim.SetBool("JumpOn", true);
+            rigd.velocity = new Vector2(0, jumpPower); // 더블점프 문제로 고정값 넣어둠
+            jumpCount--;
+        }
+    }
+}
