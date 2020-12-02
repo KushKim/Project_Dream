349eea5826b0a7df7e231936d66edd28fcc9a91e
JungheunKim <kushkim@icloud.com>
2020-12-03 03:18:15 +0900

    DeadAni Added

---
+++ Assets/Script/PlayerScript/playerdead.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class playerdead : MonoBehaviour
+{
+    public GameObject hpbar;
+    public GameObject youdie;
+    void Update()
+    {
+        
+        if (hpbar.transform.gameObject.GetComponent<Image>().fillAmount == 0)
+        {
+            Time.timeScale = 0.5f;
+            youdie.SetActive(true);
+        }
+
+    
+    }
+}
