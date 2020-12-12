2a9c0f4d9da2ed477ddf779bec0dc9d17112e11b
Hyangyoun <thakd7092@naver.com>
2020-12-12 15:55:38 +0900

    스테이지 전환 버그 수정
    페이드아웃 코드 변경

--- Assets/Script/ChangeScene.cs
+++ Assets/Script/ChangeScene.cs
@@ -8,6 +8,7 @@ public class ChangeScene : MonoBehaviour
 {
     public Image image;
     public string sceneName;
+    private float time;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +25,10 @@ public class ChangeScene : MonoBehaviour
     {
         if(collision.gameObject.tag == "Player")
         image.gameObject.SetActive(true);
-        image.color = new Color(0, 0, 0, Mathf.Lerp(image.color.a, 1, 3 * Time.deltaTime));
+        time += Time.deltaTime / 1.5f;
+        Color color = image.color;
+        color.a = Mathf.Lerp(0, 1, time);
+        image.color = color;
         if (image.color.a >= 0.9999f)
         {
             SceneManager.LoadScene(sceneName);
